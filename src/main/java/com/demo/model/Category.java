4f9bd33434137a6797fc0eb79dcbfd0de5ef24f9
Hendro Steven <hendro.steven@gmail.com>
2021-05-27 12:42:01 +0700

    add lombok

--- src/main/java/com/demo/model/Category.java
+++ src/main/java/com/demo/model/Category.java
@@ -7,8 +7,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
 @Table(name = "tbl_categories")
+@Data
 public class Category {
     
     @Id
@@ -17,22 +20,5 @@ public class Category {
 
     @Column(length=100, nullable = false, unique = true)
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     
 }
--- src/main/java/com/demo/model/Product.java
+++ src/main/java/com/demo/model/Product.java
@@ -8,8 +8,11 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.Data;
+
 @Entity
 @Table(name = "tbl_products")
+@Data
 public class Product {
     
     @Id
@@ -29,67 +32,6 @@ public class Product {
 
     @ManyToOne
     private Category category;
-
-    public Product(){}
-
-    public Product(Long id, String code, String name, String description, double price, Category category) {
-        this.id = id;
-        this.code = code;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.category = category;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-    
-    
 
     
 }
